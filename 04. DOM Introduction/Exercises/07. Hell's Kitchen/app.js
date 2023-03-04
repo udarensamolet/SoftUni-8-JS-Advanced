0739b90b74e66c00477cc2ce105d6f51a4ddbd63
Rositsa Nenova <udarensamolet@yahoo.com>
2023-03-04 03:36:26 +0200

    added solutions for exercises 07-09 from 04

--- 04. DOM Introduction/Exercises/07. Hell's Kitchen/app.js
+++ 04. DOM Introduction/Exercises/07. Hell's Kitchen/app.js
@@ -1,12 +1,55 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
+   const input = document.querySelector('#inputs>textarea');
+   const bestRestaurantText = document.querySelector('#bestRestaurant>p');
+   const workersText = document.querySelector('#workers>p');
+
    function onClick () {
-      let restaurantsInfoInput = document.getElementsByTagName("textarea")[0].value;
-      let restaurantsInfo = restaurantsInfoInput.split(', ');
-      console.log("asd");
+      const inputArray = JSON.parse(input.value);
+      
+      let restaurants = {};
+      inputArray.forEach((item) => {
+         const splitedArray = item.split(' - ');
+         const restaurant = splitedArray[0];
+   
+         let workers = [];
+         splitedArray[1].split(', ').forEach((worker) => {
+           const tokens = worker.split(' ');
+           const workerName = tokens[0];
+           const workerSalary = Number(tokens[1]);
+   
+           workers.push({workerName, workerSalary });
+         });
 
+         if(restaurants[restaurant]){
+            workers = workers.concat(restaurants[restaurant].workers);
+         }
+         
+         workers.sort((a, b) => b.workerSalary - a.workerSalary);
 
+         const bestSalary = workers[0].workerSalary;
+         const averageSalary = workers.reduce((acc, curr) => 
+            acc + curr.workerSalary, 0) / workers.length;
+         restaurants[restaurant] = {workers, bestSalary, averageSalary};
+      });
 
+      let bestAverageSalaryAll = 0;
+      let bestRestaurantAll = undefined;
+
+      for(const name in restaurants){
+         if (restaurants[name].averageSalary > bestAverageSalaryAll) {
+            bestAverageSalaryAll = restaurants[name].averageSalary;
+            bestRestaurantAll = {name, ...restaurants[name]};
+         }
+      }
+
+      bestRestaurantText.textContent = `Name: ${bestRestaurantAll.name} Average Salary: ${bestRestaurantAll.averageSalary.toFixed(2)} Best Salary: ${bestRestaurantAll.bestSalary.toFixed(2)}`;
+      let workerResult = [];
+      bestRestaurantAll.workers.forEach((worker) =>{
+         workerResult.push(`Name: ${worker.workerName} With Salary: ${worker.workerSalary.toFixed(2)}`);
+      });
+
+      workersText.textContent = workerResult.join(' ');
    }
 }
